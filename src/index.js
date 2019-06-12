76334acaa1f44853f429437bbc813beee9aa19d6
Oliver Rushworth <oliver@oliverushworth.uk>
2019-06-12 06:43:10 +0100

    [Setup] - Create File Structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './global.css';
+import HotelSearchScreen from './screens/hotel-search/hotel-search.component';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HotelSearchScreen />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/screens/hotel-search/hotel-search.test.js
+++ src/screens/hotel-search/hotel-search.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './hotel-search.component';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
