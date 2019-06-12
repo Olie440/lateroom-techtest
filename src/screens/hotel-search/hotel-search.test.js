a96dfd49accf7d1f469b57153cbd6b0f4a354e25
Oliver Rushworth <oliver@oliverushworth.uk>
2019-06-12 07:26:29 +0100

    [#8] - Install Enzyme

--- src/screens/hotel-search/hotel-search.test.js
+++ src/screens/hotel-search/hotel-search.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './hotel-search.component';
+import { shallow } from 'enzyme';
+import { App } from './hotel-search.component';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const component = shallow(<App />);
+  expect(component).toMatchSnapshot();
 });
